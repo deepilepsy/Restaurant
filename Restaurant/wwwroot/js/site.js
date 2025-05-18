0065fd4ae8f2005e002caabc17eba378a155f55a
grravitie <fineos3553@gmail.com>
2025-05-18 11:58:26 +0300

    +Homepage style update. +Example logo changed.

--- Restaurant/wwwroot/js/site.js
+++ Restaurant/wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+if (window.location.pathname.toLowerCase() === "/") {
+    window.addEventListener("scroll", function () {
+        const header = document.getElementById("site-header");
+        if (window.scrollY > 50) {
+            header.style.backgroundColor = "rgba(35, 35, 35, 0.9)"; // opaque
+        } else {
+            header.style.backgroundColor = "rgba(0, 0, 0, 0)"; // transparent
+        }
+    })
+}
 // Write your JavaScript code.
